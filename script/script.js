66815ad1424cc270bd9af44ca5bb7c04ff56fe60
Anil Korkmaz <anil.korkmaz@edu-kw1c.nl>
2021-03-04 16:13:49 +0100

    Tekst toegevoegd voor dat alle blokken tevoorschijnkomen/ Tekst gestyled en fadeOut gegeven

--- script/script.js
+++ script/script.js
@@ -9,14 +9,17 @@ $(document).ready(function () {
     $('#home').addClass('active');
   });
 
-// Fade in en fade out van de nieuws blokken bij het opladen van de pagina
+// Fade in en fade out van de nieuws blokken bij het opladen van de Nieuwspagina
 $(document).ready(function(){
-  $("#div1").fadeIn(2000);
-  $("#div2").fadeIn(3000);
-  $("#div3").fadeIn(4000);
-  $("#div4").fadeIn(5000);
-  $("#div5").fadeIn(6000);
-  $("#div6").fadeIn(6000);
-  $("#div7").fadeIn(6000);
-  $("#div8").fadeIn(6000);
+  $("#laatsteNieuws").css({"font-size": "80px", "position":"absolute", "top":"50%", "left":"5%"}).fadeOut(5000,
+    function(){
+      $("#div1").fadeIn(2000);
+      $("#div2").fadeIn(3000);
+      $("#div3").fadeIn(4000);
+      $("#div4").fadeIn(5000);
+      $("#div5").fadeIn(6000);
+      $("#div6").fadeIn(6000);
+      $("#div7").fadeIn(6000);
+      $("#div8").fadeIn(6000);
+    });
 });
