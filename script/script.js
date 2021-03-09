e39da20cef678199cc8861637500c4826af1718c
Anil Korkmaz <anil.korkmaz@edu-kw1c.nl>
2021-03-09 20:59:04 +0100

    Afbeeldingen toegevoegd voor producten pagina.  Script toegevoegd voor filter. Styling filter & p-elementen. Producten + filter toegevoegd.

--- script/script.js
+++ script/script.js
@@ -14,3 +14,24 @@ $(document).ready(function(){
       $("#div8").fadeIn(6000);
     });
 });
+
+// Filter ProductenPagina
+function change() {
+  let results = Array.from(document.querySelectorAll('.result > div'));
+  // Hide all results
+  results.forEach(function(result) {
+    result.style.display = 'none';
+  });
+  // Filter results to only those that meet ALL requirements:
+  Array.from(document.querySelectorAll('.filter input[rel]:checked'), function(input) {
+    const attrib = input.getAttribute('rel');
+    results = results.filter(function(result) {
+      return result.classList.contains(attrib);
+    });
+  });
+  // Show those filtered results:
+  results.forEach(function(result) {
+    result.style.display = 'block';
+  });
+}
+change();
